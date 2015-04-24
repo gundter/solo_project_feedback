ad2d8044c646d311f56955bbb0a2200d51d7e41b
Terry Gunderson <tgunderson25@gmail.com>
2015-04-24 11:32:27 -0500

    Adding Changes

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 /**
  * Created by Prime on 4/24/15.
  */
+var date =["4/13/2015", "4/14/2015", "4/15/2015", "4/16/2015", "4/17/2015", "4/20/2015", "4/21/2015", "4/22/2015", "4/23/2015"];
+
+
+$(document).ready(function(){
+    jQuery.get('text.txt', function(data){
+        var array = data.split("\n");
+        array.split("\t");
+        console.log(array);
+        $('#show').html(array);
+    });
+
+    $('button').click(function(){
+
+    });
+
+});
