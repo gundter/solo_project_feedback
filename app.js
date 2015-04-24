ae0f53590ef350604868335450f9f4dbf5b5ffc9
Terry Gunderson <tgunderson25@gmail.com>
2015-04-24 14:20:42 -0500

    More functionality and styling

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //var date =["4/13/2015", "4/14/2015", "4/15/2015", "4/16/2015", "4/17/2015", "4/20/2015", "4/21/2015", "4/22/2015", "4/23/2015"];
 var shoutOuts = [];
 var i;
-var counter = 0;
+//var counter = 0;
 
 $(document).ready(function(){
     jQuery.get('text.txt', function(data){
@@ -13,15 +13,32 @@ $(document).ready(function(){
         //$('#show').html(array);
     });
 
-    $('button').click(function(){
-        for (i = 0; i < shoutOuts.length; i++){
-            counter++;
-            $("#show").append("<li id=" + counter + ">" + shoutOuts[i] + "</li>");
-            console.log(counter);
-            var added = $("#" + counter);
-        }
-        console.log(added);
-        added.toggle();
+    //$('button').click(function(){
+    //    for (i = 0; i < shoutOuts.length; i++){
+    //        counter++;
+    //        $("#show").append("<li class='newThing'>" + shoutOuts[i] + "</li>");
+    //        console.log(counter);
+    //        var added = $("#" + counter);
+    //    }
+    //    console.log(added);
+    //    added.toggle();
+    //});
+
+    $('button').on("click", function(){
+       for (i = 0; i < shoutOuts.length; i++){
+           //console.log(el);
+           $('#show').append("<li class='newBullet'>" + shoutOuts[i] + "</li>");
+
+           $(".newBullet").hide().delay( i * 1000).slideDown();
+
+       }
+    });
+
+    $('ul').on('click','.newBullet', function(){
+        $(this).animate({
+            fontSize: "150%",
+            opacity: "0.6"
+        });
     });
 
 });
